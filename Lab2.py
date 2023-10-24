80e59da46f5e4dc2d0fb1578804150b01ed14430
JULIAN <JULIANHO.22@ichat.sp.edu.sg>
2023-10-24 16:41:21 +0800

    Modified Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -1,10 +1,12 @@
+import statistics
 def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
     display_main_menu()
     num_list = get_user_input()
     calc_average(num_list)
     find_min_max(num_list)
-    print(num_list)
+    calc_median_temperature(num_list)
+
 
 
 def display_main_menu():
@@ -14,11 +16,13 @@ def get_user_input():
     userinput = input()
     userlist = userinput.split(",")
     Ulist = [float(ele) for ele in userlist]
+    print(Ulist)
     return Ulist
 
 def calc_average(numlist):
     Nlist = numlist
     avgVal = sum(Nlist)/len(Nlist)
+    print(avgVal)
     return avgVal
 
 def find_min_max(numlist):
@@ -26,6 +30,16 @@ def find_min_max(numlist):
     maxVal = max(List)
     minVal = min(List)
     Intlist = [minVal,maxVal]
+    print(Intlist)
     return Intlist
+
+def calc_median_temperature(numlist):
+    Lists = numlist
+    newLists = sorted(Lists)
+    MedianVal = statistics.median(newLists)
+    print(MedianVal)
+    return MedianVal
+
+
 if __name__ == "__main__":
     main()
